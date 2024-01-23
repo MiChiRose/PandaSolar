2199fafcdd01f72b4fbddb9d3d1e8a9b6ad40334
Yura Menschikov <yura.menschikov@icloud.com>
2024-01-24 01:01:11 +0300

    refactoring gradient

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -4,11 +4,11 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { Colors } from '../constants/color';
 
 interface GradientProps {
-  children?: JSX.Element | JSX.Element[];
+  children?: React.JSX.Element | React.JSX.Element[];
   style?: StyleProp<ViewStyle>;
 }
 
-export const Gradient = ({ children, style }: GradientProps): JSX.Element => {
+export const Gradient = ({ children, style }: GradientProps): React.JSX.Element => {
   return (
     <LinearGradient colors={[Colors.gradientStart, Colors.gradientEnd]} style={[style]}>
       {children}
