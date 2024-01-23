df820be2f638c4ff88cb970f13f6bb79fa4e3a51
Yura Menschikov <yura.menschikov@icloud.com>
2024-01-24 00:58:52 +0300

    import refactoring

--- Firebase/fireStoreLocal.ts
+++ Firebase/fireStoreLocal.ts
@@ -1,17 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBvQLZU1kS7V7HdsWssgpVCxq78TUDqHDU",
-    authDomain: "pandasolar-4036f.firebaseapp.com",
-    projectId: "pandasolar-4036f",
-    storageBucket: "pandasolar-4036f.appspot.com",
-    messagingSenderId: "313158142159",
-    appId: "1:313158142159:web:1cfc6c466be7158ba14fba",
-    measurementId: "G-PRFX90H53N"
+  apiKey: 'AIzaSyBvQLZU1kS7V7HdsWssgpVCxq78TUDqHDU',
+  authDomain: 'pandasolar-4036f.firebaseapp.com',
+  projectId: 'pandasolar-4036f',
+  storageBucket: 'pandasolar-4036f.appspot.com',
+  messagingSenderId: '313158142159',
+  appId: '1:313158142159:web:1cfc6c466be7158ba14fba',
+  measurementId: 'G-PRFX90H53N',
 };
 
-initializeApp(firebaseConfig)
+initializeApp(firebaseConfig);
 
 const dataBase = getFirestore();
 
