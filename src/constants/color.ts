7faee9695fe456ae4a7e4373e853eb5df4ff8b31
Yura Menschikov <yura.menschikov@icloud.com>
2024-01-24 00:59:55 +0300

    remove unused colors

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,10 +1,6 @@
 export const Colors = {
-  mainBlue: '#0060AB',
   black: '#000000',
   white: '#FFFFFF',
-  listItem: '#EAEAEA',
-  grey_8F: '#8F8F8F',
-  detailsButton: '#161973',
   transparent: 'transparent',
   gradientStart: '#9216BD',
   gradientEnd: '#D531E3',
