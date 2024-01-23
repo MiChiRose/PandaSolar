1c8149f8415f8fc0bb0abaa7e84015bb592616c4
Yura Menschikov <yura.menschikov@icloud.com>
2024-01-24 01:00:41 +0300

    refactoring screen

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,38 +1,36 @@
 import React, { memo, useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import Spinner from 'react-native-loading-spinner-overlay';
-import Container from '../components/Container';
+import Container from '../components/container';
 import { MapViewV1 } from '../components/MapViewV1';
-import { CustomScrollView } from '../components/CustomScrollView';
-import { getData } from '../components/data';
+import CustomScrollView from '../components/scrollView';
+import { getData } from '../utils/data';
 
-const ContactsScreen = (): JSX.Element => {
+const ContactsScreen = (): React.JSX.Element => {
   const [loading, isLoading] = useState(false);
 
-  // const dataLoad = () => {
-  //   isLoading(true);
-  //   getData({ mainPath: 'main', documentPath: 'contacts' })
-  //     .then((resp) => {
-  //       // setAddressData(resp[0]);
-  //       // setContactsData(resp[1]);
-  //       isLoading(false);
-  //     })
-  //     .catch((e) => {
-  //       isLoading(false);
-  //       console.log(e);
-  //     });
-  // };
-  //
-  // useEffect(() => {
-  //   dataLoad();
-  // }, []);
+  const dataLoad = () => {
+    // isLoading(true);
+    // getData({ mainPath: 'main', documentPath: 'contacts' })
+    //   .then((resp) => {
+    //     // setAddressData(resp[0]);
+    //     // setContactsData(resp[1]);
+    //     isLoading(false);
+    //   })
+    //   .catch((e) => {
+    //     isLoading(false);
+    //     console.log(e);
+    //   });
+  };
+
+  useEffect(() => {
+    dataLoad();
+  }, []);
 
   return (
     <Container>
-      <CustomScrollView refreshing={loading} refresh={() => {}}>
+      <CustomScrollView refreshing={loading} refresh={dataLoad}>
         <MapViewV1 />
       </CustomScrollView>
-      <Spinner visible={loading} />
     </Container>
   );
 };
