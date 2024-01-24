29aff6294f686f6eeb49ea302c08060ddd20a684
Yura Menschikov <yura.menschikov@icloud.com>
2024-01-24 16:34:16 +0300

    add new data types

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -14,4 +14,58 @@ type PriceData = {
   price: string;
 };
 
-export { RulesType, PriceList, PriceData };
+type ContactsAddressType = {
+  address: string;
+  header: string;
+};
+
+type ContactsPhone = {
+  image: string;
+  phone: string;
+};
+
+type ContactsSocialWeb = {
+  image: string;
+  link?: string;
+};
+
+type ContactsSocial = {
+  header: string;
+  instagram: ContactsSocialWeb;
+  vk: ContactsSocialWeb;
+  telegram: ContactsSocialWeb;
+};
+
+type ContactsType = {
+  header: string;
+  address: ContactsAddressType;
+  phoneFirst: ContactsPhone;
+  phoneSecond: ContactsPhone;
+  social: ContactsSocial;
+};
+
+type NewsType = {
+  image: string;
+  title: string;
+  link?: string;
+};
+
+type InviteAddress = {
+  address: string;
+  title: string;
+};
+
+type InviteHours = {
+  time: string;
+  title: string;
+};
+
+type InviteType = {
+  header: string;
+  title: string;
+  subtitle: string;
+  address: InviteAddress;
+  hours: InviteHours;
+};
+
+export { RulesType, PriceList, PriceData, ContactsType, NewsType, InviteType };
